fb6ec8c55068c42cb2066e8bd417879581168fb7
devlog98 <dno1998@hotmail.com>
2020-07-11 16:44:26 -0300

    Basic Player movement

---
+++ Assets/2-Scripts/Aim.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace devlog98.Mouse {
+    public class Aim : MonoBehaviour {
+        public static Aim instance; // singleton
+
+        private Vector2 mousePosition; // cursor position onscreen
+
+        // singleton setup
+        private void Awake() {
+            if (instance != null && instance != this) {
+                Destroy(this.gameObject);
+            }
+            else {
+                instance = this;
+            }
+        }
+
+        // unlock and hide mouse cursor
+        private void Start() {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = false;
+        }
+
+        // update cursor position
+        private void Update() {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = mousePosition;
+        }
+
+        // return direction based on position
+        public Vector2 GetAimDirection(Vector2 position) {
+            return (mousePosition - position).normalized; 
+        }
+    }
+}
---
+++ Assets/2-Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using devlog98.Mouse;
+
+namespace devlog98.Player {
+    public class Player : MonoBehaviour {
+        [Header("Jump")]
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private float jumpSpeed; // jump speed
+        
+        private bool canJump = true; // if player can jump
+
+        private void Update() {
+            // jump input
+            if (canJump && Input.GetMouseButtonDown(0)) {
+                Vector2 jumpDirection = Aim.instance.GetAimDirection(transform.position);
+                Jump(jumpDirection);
+                canJump = false;
+            }
+        }
+
+        // jumps in a given direction
+        public void Jump(Vector2 jumpDirection) {
+            rb.velocity = jumpDirection * jumpSpeed;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision) {
+            // stops jump on collisions
+            rb.velocity = Vector2.zero;
+            canJump = true;
+        }
+    }
+}
