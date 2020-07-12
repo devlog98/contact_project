d435adeda06ad5023ee9f6e51a33a0b1a84fdf73
devlog98 <dno1998@hotmail.com>
2020-07-12 02:35:16 -0300

    Added 2D extras, Wall brush and trying to fix Player movement collisions

--- Assets/2-Scripts/Player.cs
+++ Assets/2-Scripts/Player.cs
@@ -10,38 +10,72 @@ namespace devlog98.Player {
         [Header("Jump")]
         [SerializeField] private Rigidbody2D rb;
         [SerializeField] private float jumpSpeed; // jump speed
+        private Vector2 jumpDirection;
+
 
         [Header("Collision")]
         [SerializeField] private LayerMask collisionMask;
-
         private bool canJump = true; // if player can jump
+        private bool isJumping;
 
         private void Update() {
             // jump input
             if (canJump && Input.GetMouseButtonDown(0)) {
                 // jump
-                Vector2 jumpDirection = Aim.instance.GetAimDirection(transform.position);
+                jumpDirection = Aim.instance.GetAimDirection(transform.position);
                 Jump(jumpDirection);
-                
+            }
+            else if (isJumping) {
+                // land on collision
+                JumpCollisionResult check = JumpCollisionCheck(jumpDirection, 0.64f);
+                if (!check.Success) {
+                    Land(check.CollisionPoint);
+                }
             }
         }
 
         // jumps in a given direction
         public void Jump(Vector2 jumpDirection) {
-            // rotate sprite
-            float rotation = Mathf.Atan2(-jumpDirection.y, -jumpDirection.x) * Mathf.Rad2Deg;
-            sprite.rotation = Quaternion.Euler(0f, 0f, rotation + 90f);
+            // only if jump is viable
+            if (JumpCollisionCheck(jumpDirection, 2.56f).Success) {
+                // rotate sprite
+                float rotation = Mathf.Atan2(-jumpDirection.y, -jumpDirection.x) * Mathf.Rad2Deg;
+                sprite.rotation = Quaternion.Euler(0f, 0f, rotation + 90f);
 
-            // jump
-            rb.velocity = jumpDirection * jumpSpeed;
-            canJump = false;
+                // jump
+                rb.velocity = jumpDirection * jumpSpeed;
+                canJump = false;
+                isJumping = true;
 
-            // jump anim
-            anim.SetBool("canJump", canJump);
+                // jump anim
+                anim.SetBool("canJump", canJump);
+            }
+        }
+
+        // checks if jump is possible
+        private JumpCollisionResult JumpCollisionCheck(Vector2 jumpDirection, float checkDistance) {
+            bool success = false;
+            Vector2 collisionPoint = Vector2.zero;
+
+            // cast multiple rays
+            for (int i = -1; i <= 1; i++) {
+                RaycastHit2D hit = Physics2D.Raycast(transform.position + Vector3.right * 0.32f * i, jumpDirection, checkDistance, collisionMask);
+
+                if (hit.collider != null) {
+                    collisionPoint = hit.collider.ClosestPoint(transform.position);
+                    break;
+                }
+            }
+
+            if (collisionPoint == Vector2.zero) {
+                success = true;
+            }
+
+            return new JumpCollisionResult(success, collisionPoint);
         }
 
         // lands on the correct rotation
-        public void Land(Vector2 landPoint) {
+        private void Land(Vector2 landPoint) {
             // calculate distance on x and y axis
             float yDistance = Mathf.Abs(landPoint.y - transform.position.y);
             float xDistance = Mathf.Abs(landPoint.x - transform.position.x);
@@ -68,14 +102,31 @@ namespace devlog98.Player {
             // landing
             rb.velocity = Vector2.zero;
             canJump = true;
+            isJumping = false;
 
             // landing anim
             anim.SetBool("canJump", canJump);
         }
 
+
+
         // land on collision
-        private void OnTriggerEnter2D(Collider2D collision) {
-            Land(collision.ClosestPoint(transform.position));
+        //private void OnTriggerEnter2D(Collider2D collision) {
+        //    Land(collision.ClosestPoint(transform.position));
+        //}
+    }
+
+    // special struct to use on jump checks
+    public struct JumpCollisionResult {
+        private bool success;
+        private Vector2 collisionPoint;
+
+        public bool Success { get => success; }
+        public Vector2 CollisionPoint { get => collisionPoint; }
+
+        public JumpCollisionResult(bool _success, Vector2 _collisionPoint) {
+            success = _success;
+            collisionPoint = _collisionPoint;
         }
     }
 }
