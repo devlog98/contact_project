5ea52d46dcc901dea40b598d08457aa26fa21ede
devlog98 <dno1998@hotmail.com>
2020-08-29 12:36:52 -0300

    Cleaning Player and Bullet scripts

--- Assets/2-Scripts/Bullet.cs
+++ Assets/2-Scripts/Bullet.cs
@@ -3,11 +3,38 @@
 namespace devlog98.Ammunition {
     public class Bullet : MonoBehaviour {
         [SerializeField] Rigidbody2D rb;
-        [SerializeField] float moveSpeed;
+        [SerializeField] float moveSpeed; // speed of bullet
+        [SerializeField] int duration; // duration of bullet on scene
+
+        private Transform parent; // bullet parent
 
         // shoots bullet based on direction
-        public void Shoot(Vector2 moveDirection) {
+        public void Shoot(Vector2 startPosition, Vector2 moveDirection) {
+            // store parent
+            parent = transform.parent;
+            transform.SetParent(null);
+
+            // shoot
+            this.gameObject.SetActive(true);
+            transform.position = startPosition;
             rb.velocity = moveDirection * moveSpeed;
+
+            // set bullet duration
+            CancelInvoke("Disable");
+            Invoke("Disable", duration);
+        }
+
+        // collision checks
+        private void OnTriggerEnter2D(Collider2D other) {
+            if (other.gameObject.tag != "Player") {
+                Disable();
+            }
+        }
+
+        // disable bullet
+        private void Disable() {
+            transform.parent = parent;
+            this.gameObject.SetActive(false);
         }
     }
 }
--- Assets/2-Scripts/Player.cs
+++ Assets/2-Scripts/Player.cs
@@ -2,82 +2,69 @@
 using devlog98.Mouse;
 using System.Collections;
 using devlog98.UI;
-using UnityEngine.SceneManagement;
 using devlog98.Audio;
 using devlog98.Ammunition;
+using System.Collections.Generic;
 
 namespace devlog98.Player {
     public class Player : MonoBehaviour {
-        [Header("General")]
-        [SerializeField] private Animator anim;
-        [SerializeField] private Transform sprite;
+        [Header("Movement")]
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private Collider2D collider;
+        private Vector2 currentDirection = Vector2.zero; // movement direction
+        private float currentSpeed; // movement speed
 
         [Header("Jump")]
-        [SerializeField] private Rigidbody2D rb;
-        [SerializeField] private float jumpSpeed; // jump speed
-        [SerializeField] private float jumpSpeedBonus;
-        public float currentJumpSpeed;
-
-        [SerializeField] private int maxJumps;
-        private int jumps;
+        [SerializeField] private int maxJumpCount; // number of times player can jump without touching ground
+        [SerializeField] private float jumpSpeed; // default jump speed
+        [SerializeField] private float jumpSpeedBonus; // speed increase when chaining multiple jumps
+        [SerializeField] private float jumpSpeedAngle; // max angle difference for bonus speed to be applied
+        private int jumpCount; // how many times has the player jumped right now
 
         [Header("Shoot")]
-        [SerializeField] private Bullet bullet;
+        [SerializeField] private List<Bullet> bullets; // list of player ammunition
+        [SerializeField] private float shootSpeed; // default shoot speed
+        [SerializeField] private float shootSpeedBonus; // speed increase when chaining multiple shots
+        [SerializeField] private float shootSpeedAngle; // max angle difference for bonus speed to be applied
+
+        [Header("Collision")]
+        [SerializeField] private LayerMask collisionMask; // mask that can collide with player
+        [SerializeField] private float collisionDistance; // how far mask check will go
+        private float collisionTolerance = .1f; // period collider will be turned off for smooth launching
+        private bool isGrounded = true; // if player is on the ground
+
+        [Header("Animation")]
+        [SerializeField] private Animator anim;
+        [SerializeField] private Transform sprite;
 
         [Header("Audio")]
         [SerializeField] private AudioClip jumpClip;
         [SerializeField] private AudioClip landClip;
         [SerializeField] private AudioClip deathClip;
 
-        private Vector2 currentDirection;
-        private Vector2 jumpDirection;
-        private Vector2 shootDirection;
-
-        private int landCounter;
-
-        [Header("Collision")]
-        [SerializeField] private Collider2D collider;
-        [SerializeField] private LayerMask collisionMask;
-        [SerializeField] private float collisionThreshold;
-
-        private bool isLanded = true; // if player is landed on the ground
-
-        private float collisionTime;
-        private float collisionTolerance = .1f;
-
-        // initial setup
-        private void Start() {
-            currentDirection = Vector2.zero;
-            currentJumpSpeed = jumpSpeed;
-        }
+        private int landCounter; // times player landed on the ground
 
         private void Update() {
-            // jump input
-            if (jumps < maxJumps && Input.GetMouseButtonDown(0)) {
-                // jump
-                jumpDirection = Aim.instance.GetAimDirection(transform.position);
+            // jump
+            if (Input.GetMouseButtonDown(0)) {
+                Vector2 jumpDirection = Aim.instance.GetAimDirection(transform.position);
                 Jump(jumpDirection);
             }
 
-            // shoot input
+            // shoot
             if (Input.GetMouseButtonDown(1)) {
-                // shoot
-                shootDirection = Aim.instance.GetAimDirection(transform.position);
+                Vector2 shootDirection = Aim.instance.GetAimDirection(transform.position);
                 Shoot(shootDirection);
             }
 
-            CheckDeath();
+            CheckOutOfBounds();
         }
 
         // jumps in a given direction
         private void Jump(Vector2 jumpDirection) {
-            // only if jump is viable
-            if (!isLanded || JumpCollisionCheck(jumpDirection)) {
-                // jump
-                Launch(jumpDirection);
-                jumps++;
-
-                if (isLanded) {
+            // jump if possible
+            if (JumpCheck(jumpDirection)) {
+                if (isGrounded) {
                     // unparent from land object
                     transform.SetParent(null);
 
@@ -85,61 +72,71 @@ namespace devlog98.Player {
                     StopCoroutine(DisableCollider());
                     StartCoroutine(DisableCollider());
 
-                    isLanded = false;
+                    isGrounded = false;
                 }
 
-                // jump anim
-                anim.SetBool("canJump", isLanded);
+                // jump
+                Launch(jumpDirection, jumpSpeed, jumpSpeedBonus, jumpSpeedAngle);
+                jumpCount++;
+
+                // jump feedback
+                anim.SetBool("canJump", isGrounded);
                 AudioManager.instance.PlayClip(jumpClip);
             }
         }
 
         // checks if jump is possible
-        private bool JumpCollisionCheck(Vector2 jumpDirection) {
-            bool success = false;
-
-            Debug.Log(Vector2.Angle(jumpDirection, transform.up));
-            float jumpAngle = Vector2.Angle(jumpDirection, transform.up);
-            if (jumpAngle < 90) {
-                RaycastHit2D hit = Physics2D.Raycast(transform.position, jumpDirection, collisionThreshold, collisionMask);
-                if (hit.collider == null) {
-                    success = true;
-                }
+        private bool JumpCheck(Vector2 jumpDirection) {
+            // jump counter check
+            if (jumpCount >= maxJumpCount) {
+                return false;
             }
-            //else if (jumpAngle < 125) {
-            //    RaycastHit2D hit = Physics2D.Raycast(transform.position, jumpDirection, 2.56f, collisionMask);
-            //    if (hit.collider == null) {
-            //        success = true;
-            //    }
-            //}
+            // jump in mid air check
+            else if (!isGrounded) {
+                return true;
+            }
+            // jump in ground check
+            else {
+                float jumpAngle = Vector2.Angle(jumpDirection, transform.up); // jumpDirection angle based on player transform
 
-            return success;
+                if (jumpAngle < 90) {
+                    RaycastHit2D hit = Physics2D.Raycast(transform.position, jumpDirection, collisionDistance, collisionMask);
+
+                    // if no object will collide with player jump
+                    if (hit.collider == null) {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
         }
 
         // lands on the correct rotation
         private void Land(Transform landTransform, Vector2 landPoint) {
             // parent to land object
             transform.SetParent(landTransform);
+            isGrounded = true;
+
+            // landing
+            rb.velocity = Vector2.zero;
+            landCounter++;
+
+            // landing feedback
+            anim.SetBool("canJump", isGrounded);
+            AudioManager.instance.PlayClip(landClip);
 
             // rotate sprite
             Vector2 landDirection = (landPoint - (Vector2)transform.position).normalized;
             float rotation = Mathf.Atan2(-landDirection.y, -landDirection.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, rotation - 90f);
 
-            // landing
-            rb.velocity = Vector2.zero;
-            isLanded = true;
-            jumps = 0;
-
-            // landing anim
-            anim.SetBool("canJump", isLanded);
-            AudioManager.instance.PlayClip(landClip);
-
-            // reset speed
-            currentJumpSpeed = jumpSpeed;
+            // reset movement
+            jumpCount = 0;
             currentDirection = Vector2.zero;
+            currentSpeed = 0;
 
-            landCounter++;
+            // update land counter
             UIController.instance.UpdateCounter(landCounter);
         }
 
@@ -151,72 +148,82 @@ namespace devlog98.Player {
         }
 
         private void Shoot(Vector2 shootDirection) {
-            // instantiate bullet
-            Bullet newBullet = Instantiate(bullet, transform.position, transform.rotation);
-            newBullet.Shoot(shootDirection);
+            // get bullet
+            Bullet bullet = bullets.Find(x => !x.gameObject.activeSelf);
 
-            // launch player if in mid air
-            if (!isLanded) {
-                Launch(shootDirection * -1);
+            // shoot if bullet is available
+            if (bullet != null) {
+                bullet.Shoot(transform.position, shootDirection);
+
+                // launch player if in mid air
+                if (!isGrounded) {
+                    shootDirection = shootDirection * -1;
+                    Launch(shootDirection, shootSpeed, shootSpeedBonus, shootSpeedAngle);
+                }
             }
         }
 
         // launches player in direction
-        private void Launch(Vector2 direction) {
+        private void Launch(Vector2 direction, float speed, float speedBonus, float angle) {
             // rotate sprite
             float rotation = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, rotation + 90f);
 
             // modify speed depending on movement angle
-            if (Vector2.Angle(direction, currentDirection) < 30) {
-                currentJumpSpeed += jumpSpeedBonus;
+            float directionAngle = Vector2.Angle(direction, currentDirection);
+            if (currentSpeed == 0 || directionAngle > angle) {
+                currentSpeed = speed;
             }
             else {
-                currentJumpSpeed = jumpSpeed;
+                currentSpeed += speedBonus;
             }
 
             // launch
-            rb.velocity = direction * currentJumpSpeed;
+            rb.velocity = direction * currentSpeed;
 
             // set new direction
             currentDirection = direction;
         }
 
         // player death
-        private void CheckDeath() {
+        private void CheckOutOfBounds() {
             // out of bounds
             if (transform.position.x < -10.24 || transform.position.x > 10.24 || transform.position.y < -5.12 || transform.position.y > 6.4) {
-                // reset parent
-                transform.SetParent(null);
-
-                // reset position
-                transform.position = new Vector3(0f, -3.65f, 0f);
-                transform.rotation = Quaternion.Euler(Vector3.zero);
-
-                // rest jump
-                isLanded = true;
-
-                // reset velocity
-                rb.velocity = Vector3.zero;
-
-                // reset counter
-                landCounter = 0;
-                UIController.instance.UpdateCounter(landCounter);
-
-                AudioManager.instance.PlayClip(deathClip);
+                Die();
             }
         }
 
         // land on collision
         private void OnTriggerEnter2D(Collider2D collision) {
-            if (!isLanded) {
+            if (!isGrounded && collision.gameObject.tag != "Bullet") {
                 Land(collision.gameObject.transform, collision.ClosestPoint(transform.position));
             }
         }
 
+        // player death
+        private void Die() {
+            // death
+            rb.velocity = Vector3.zero; // reset velocity
+
+            transform.position = new Vector3(0f, -3.65f, 0f); // reset position
+            transform.rotation = Quaternion.Euler(Vector3.zero);
+
+            transform.SetParent(null); // reset parent
+            
+            isGrounded = true; // reset jump
+
+            // death feedback
+            AudioManager.instance.PlayClip(deathClip);
+
+            // reset land counter
+            landCounter = 0;
+            UIController.instance.UpdateCounter(landCounter);
+        }
+
+        // debug
         private void OnDrawGizmos() {
             Gizmos.color = Color.green;
-            Gizmos.DrawRay(transform.position, jumpDirection * collisionThreshold);
+            Gizmos.DrawRay(transform.position, currentDirection * collisionDistance);
 
             Gizmos.color = Color.red;
             Gizmos.DrawRay(transform.position, transform.up * 1.28f);
